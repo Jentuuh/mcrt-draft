b0676d569e3024840d0e8f9ed7a8f5b0b8c5509c
Jente Vandersanden <jentevandersanden2009@live.be>
2022-10-20 19:22:47 +0200

    Added area lights

--- mcrt-experiments/mcrt-experiments/renderer.cpp
+++ mcrt-experiments/mcrt-experiments/renderer.cpp
@@ -9,35 +9,6 @@
 #include <iostream>
 
 namespace mcrt {
-    extern "C" char embedded_ptx_code[];
-
-    // SBT record for a raygen program
-    struct __align__(OPTIX_SBT_RECORD_ALIGNMENT) RaygenRecord
-    {
-        __align__(OPTIX_SBT_RECORD_ALIGNMENT) char header[OPTIX_SBT_RECORD_HEADER_SIZE];
-        // just a dummy value - later examples will use more interesting
-        // data here
-        void* data;
-    };
-
-    // SBT record for a miss program
-    struct __align__(OPTIX_SBT_RECORD_ALIGNMENT) MissRecord
-    {
-        __align__(OPTIX_SBT_RECORD_ALIGNMENT) char header[OPTIX_SBT_RECORD_HEADER_SIZE];
-        // just a dummy value - later examples will use more interesting
-        // data here
-        void* data;
-    };
-
-    // SBT record for a hitgroup program
-    struct __align__(OPTIX_SBT_RECORD_ALIGNMENT) HitgroupRecord
-    {
-        __align__(OPTIX_SBT_RECORD_ALIGNMENT) char header[OPTIX_SBT_RECORD_HEADER_SIZE];
-        // just a dummy value - later examples will use more interesting
-        // data here
-        MeshSBTData data;
-    };
-
 
     Renderer::Renderer(Scene& scene, const Camera& camera): renderCamera{camera}, scene{scene}
     {
